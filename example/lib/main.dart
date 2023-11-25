d7aecfbe5696d946908a09cc9b71007fcff31bf7
NouzNoushad <77887277+NouzNoushad@users.noreply.github.com>
2023-11-25 20:06:00 +0530

    flutter ticket clippers

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,3 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-Flutter package that provides you clippers to create tickets.
-
-## Getting started
-
-To use this plugin, add flutter_custom_clippers as a [dependency in your pubspec.yaml file](https://flutter.io/docs/development/packages-and-plugins/using-packages).
-
-## Example
-
-```dart
 import 'package:flutter/material.dart';
 import 'package:ticket_clippers/ticket_clippers.dart';
 
@@ -129,4 +107,3 @@ class MyTicketClipper extends StatelessWidget {
     );
   }
 }
-```
---
+++ lib/ticket_clippers.dart
@@ -0,0 +1,5 @@
+// library ticket_clippers;
+export 'src/edge_enum.dart';
+export 'src/pointed_edge.dart';
+export 'src/rounded_edge.dart';
+export 'src/ticket_edge.dart';
